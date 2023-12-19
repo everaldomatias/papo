0e6fcaafa94d8f5912be7d1228c70e12ee921795
Everaldo Matias <matiaseveraldo@gmail.com>
2023-12-18 22:10:27 -0300

    Add React

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EntradaComponente from './EntradaComponente';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <EntradaComponente />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
